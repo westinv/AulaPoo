4afd1d185339c9081da61286c367bb1f946b4cb5
westinv <43423127+westinv@users.noreply.github.com>
2019-02-14 11:54:04 -0200

    primeiro commit

---
+++ Aula POO/Aula02/src/aula02/Teste.java
@@ -0,0 +1,10 @@
+package aula02;
+
+public class Teste {
+	public static void main(String[] args) {
+		aula02 p1;
+		p1 = new aula02();
+		p1.setNome("victor");
+		System.out.println(p1.getNome());
+	}
+}
---
+++ Aula POO/Aula02/src/aula02/aula02.java
@@ -0,0 +1,33 @@
+package aula02;
+
+public class aula02 {
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	String nome;
+	int idade;
+	double altura;
+	double peso;
+}
+
